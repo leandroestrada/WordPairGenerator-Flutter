1c60ff37a84f334f256849a5a9ab3af74b643224
leandroestrada1 <leandro.a.estrada@gmail.com>
2020-02-04 13:21:08 -0300

    List now working

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'package:flutter/semantics.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return MaterialApp( 
-      theme: ThemeData(primaryColor: Colors.purple[900]),
-      home: RandomWords());
-    }
-}
 
 class RandomWords extends StatefulWidget{
   @override 
